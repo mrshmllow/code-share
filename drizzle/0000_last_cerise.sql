64a425ab0c7b1540a202a8c18df8a79b006e333e
marshmallow <marshycity@gmail.com>
2023-05-24 19:10:36 +1000

    thirty one

--- drizzle/0000_last_cerise.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS "gists" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" text,
-	"text" text
-);
--- drizzle/0005_calm_human_fly.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "gists" ALTER COLUMN "id" SET DATA TYPE uuid;
-ALTER TABLE "gists" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();
