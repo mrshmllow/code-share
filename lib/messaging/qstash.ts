2e8824d47350edb7879d1b4595b4d656f0e1e222
marshmallow <marshycity@gmail.com>
2023-05-24 11:40:06 +1000

    fourteen

--- lib/messaging/qstash.ts
+++ lib/messaging/qstash.ts
@@ -1,8 +1,6 @@
 import { env } from "@/app/env.mjs";
 import { Client } from "@upstash/qstash"
 
-const qstash = new Client({
+export const qstash = new Client({
   token: env.QSTASH_TOKEN,
 });
-
-export default qstash;
