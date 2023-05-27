a0175d82dfc23a25fbcd6472744ce26ab07ca967
marshmallow <marshycity@gmail.com>
2023-05-27 10:27:14 +1000

    fourty

--- wasm/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use sanitize_html::rules::predefined::DEFAULT;
-use sanitize_html::sanitize_str;
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub fn sanitize_html(text: &str) -> String {
-    sanitize_str(&DEFAULT, text).unwrap()
-}
