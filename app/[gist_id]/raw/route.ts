173ab2955f5818443269beb8ba1d74b8d810b07e
marshmallow <marshycity@gmail.com>
2023-06-17 14:23:54 +1000

    fourty two

---
+++ app/[gist_id]/raw/route.ts
@@ -0,0 +1,17 @@
+import { db } from "@/db/db";
+import { gists } from "@/db/schema";
+import { eq } from "drizzle-orm";
+
+export async function GET(_: Request,
+  { params: { gist_id } }: { params: { gist_id: string } }
+) {
+  const gist = await db.query.gists.findFirst({
+    where: eq(gists.id, gist_id),
+  });
+
+  if (gist === undefined) {
+    return new Response("Gist not found", { status: 404 });
+  }
+
+  return new Response(gist.text);
+}
