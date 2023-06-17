3850b9e2d29b1b9c9436d19cd549f17b2841dcbb
marshmallow <marshycity@gmail.com>
2023-06-17 14:01:36 +1000

    fourty one

--- app/design/form/TextInput.tsx
+++ app/design/form/TextInput.tsx
@@ -1,5 +1,5 @@
 import { VariantProps, cva } from "cva";
-import { ReactNode } from "react";
+import { ReactNode, forwardRef } from "react";
 
 const textInput = cva(
   "px-4 min-h-[2.5rem] rounded-lg inline-flex items-center gap-3 outline-none ring-offset-slate-950 focus-visible:ring-2 ring-offset-2 border-slate-700 border text-slate-300 [&:has(:focus)]:border-blue-500",
@@ -10,36 +10,45 @@ const textInput = cva(
         false:
           "hover:brightness-95 hover:text-slate-50 from-slate-900 to-slate-800 bg-gradient-to-br",
       },
+      full: {
+        true: "w-full"
+      }
     },
     defaultVariants: {
       disabled: false,
+      full: false,
     },
   }
 );
 
 interface TextInputProps
-  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "disabled">,
+  extends Omit<React.ComponentPropsWithoutRef<'input'>, "disabled">,
   VariantProps<typeof textInput> {
   icon?: ReactNode;
 }
 
-export default function TextInput({
+const TextInput = forwardRef<HTMLInputElement, TextInputProps>(function({
   disabled,
   icon,
+  full,
   ...extra
-}: TextInputProps) {
+}, ref) {
   return (
-    <div className={textInput({ disabled })}>
+    <div className={textInput({ disabled, full })}>
       {icon !== undefined && (
         <span className="w-6 h-6 inline-flex items-center">{icon}</span>
       )}
 
       <input
         type="text"
-        className="bg-inherit outline-none placeholder:text-slate-400"
+        ref={ref}
+        className="bg-inherit outline-none placeholder:text-slate-400 w-full"
         disabled={disabled !== null ? disabled : undefined}
         {...extra}
       />
     </div>
   );
-}
+});
+TextInput.displayName = "TextInput";
+
+export default TextInput;
