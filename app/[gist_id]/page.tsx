443dabb45e530ff3d399b58113b38285da48e55a
marshmallow <marshycity@gmail.com>
2023-06-18 15:50:28 +1000

    fix edit name not being form

--- app/[gist_id]/page.tsx
+++ app/[gist_id]/page.tsx
@@ -5,31 +5,37 @@ import { useContext, useRef, useState } from "react";
 import { GistContext } from "./store";
 import TextInput from "../design/form/TextInput";
 import Button from "../design/button/Button";
-import { experimental_useOptimistic as useOptimistic } from 'react'
+import { experimental_useOptimistic as useOptimistic } from "react";
 import { updateName } from "./actions";
 import { ClipboardIcon } from "@heroicons/react/24/outline";
 
 export default function GistPage() {
   const { gist, html } = useContext(GistContext);
-  const [editing, setEditing] = useState(false)
-  const [optimisticName, optimisticUpdateName] = useOptimistic<string | null, string | null>(
-    gist.name,
-    (_, name) => name,
-  )
+  const [editing, setEditing] = useState(false);
+  const [optimisticName, optimisticUpdateName] = useOptimistic<
+    string | null,
+    string | null
+  >(gist.name, (_, name) => name);
 
-  const text = useRef<HTMLInputElement>(null)
+  const text = useRef<HTMLInputElement>(null);
 
   return (
     <div>
       <div className="border border-slate-700 rounded-lg">
         <div className="border-b border-slate-700 p-2 flex justify-between">
-          {!editing ?
+          {!editing ? (
             <>
-              <span aria-hidden="true" className="sr-only">{optimisticName}</span>
+              <span aria-hidden="true" className="sr-only">
+                {optimisticName}
+              </span>
 
-              <button className="inline-flex font-mono hover:bg-slate-900 gap-4 px-4 py-2 rounded-lg shrink items-center text-slate-300 hover:text-white" aria-label="Edit gist title" onClick={() => {
-                setEditing(true)
-              }}>
+              <button
+                className="inline-flex font-mono hover:bg-slate-900 gap-4 px-4 py-2 rounded-lg shrink items-center text-slate-300 hover:text-white"
+                aria-label="Edit gist title"
+                onClick={() => {
+                  setEditing(true);
+                }}
+              >
                 {!optimisticName ? (
                   <>
                     <span>~/generting gist name</span>
@@ -43,37 +49,50 @@ export default function GistPage() {
                 )}
               </button>
 
-              <Button aria-label="Copy gist to clipboard" onClick={async () => {
-                await navigator.clipboard.writeText(gist.text);
-              }}>
+              <Button
+                aria-label="Copy gist to clipboard"
+                onClick={async () => {
+                  await navigator.clipboard.writeText(gist.text);
+                }}
+              >
                 <Button.Icon>
                   <ClipboardIcon />
                 </Button.Icon>
               </Button>
-            </> :
-            <div className="flex gap-2 w-full">
-              <TextInput className="font-mono" autoFocus placeholder="eg. main.py" full ref={text} defaultValue={optimisticName ? optimisticName : ""} />
-
-              <Button intent="secondary" onClick={() => setEditing(false)}>
-                <Button.Text>Cancel</Button.Text>
-              </Button>
-
-              <Button intent="primary" onClick={async () => {
+            </>
+          ) : (
+            <form
+              className="flex gap-2 w-full"
+              onSubmit={async () => {
                 if (text.current === null) return;
-                console.log(text.current.value)
 
-                optimisticUpdateName(text.current.value)
-                setEditing(false)
+                optimisticUpdateName(text.current.value);
+                setEditing(false);
 
                 await updateName({
                   id: gist.id,
                   name: text.current.value,
-                })
-              }}>
+                });
+              }}
+            >
+              <TextInput
+                className="font-mono"
+                autoFocus
+                placeholder="eg. main.py"
+                full
+                ref={text}
+                defaultValue={optimisticName ? optimisticName : ""}
+              />
+
+              <Button intent="primary" className="order-last" type="submit">
                 <Button.Text>Save</Button.Text>
               </Button>
-            </div>
-          }
+
+              <Button intent="secondary" onClick={() => setEditing(false)}>
+                <Button.Text>Cancel</Button.Text>
+              </Button>
+            </form>
+          )}
         </div>
 
         <div
