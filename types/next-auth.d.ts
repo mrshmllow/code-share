544a6450c6334eff78390b631478a8594bb34a9a
marshmallow <marshycity@gmail.com>
2023-06-18 15:49:38 +1000

    fix user not being optional in overwrite

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,8 +1,9 @@
 import NextAuth, { DefaultSession } from "next-auth"
+import { DefaultJWT } from "next-auth/jwt"
 
 declare module "next-auth" {
   interface Session {
-    user: {
+    user?: {
       id: string
     } & DefaultSession["user"]
   }
