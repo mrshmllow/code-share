8d56889d6bbbc280ceb651c7d0c20cc1798a94be
marshmallow <marshycity@gmail.com>
2023-07-02 21:11:43 +1000

    Fix prod checks

--- app/(default)/search/SearchProvider.tsx
+++ app/(default)/search/SearchProvider.tsx
@@ -5,8 +5,19 @@ import { ReactNode } from "react";
 import { InstantSearch } from "react-instantsearch-hooks-web";
 import { env } from "../../env.mjs";
 
-const searchClient = algoliasearch(env.NEXT_PUBLIC_ALGOLIA_ID, '2b64a02ac721d2a4e2fc9663344f65b7');
+const searchClient = algoliasearch(
+  env.NEXT_PUBLIC_ALGOLIA_ID,
+  "2b64a02ac721d2a4e2fc9663344f65b7"
+);
+const IS_PROD = process.env.NODE_ENV == "production";
 
 export default function SearchProvider({ children }: { children: ReactNode }) {
-  return <InstantSearch searchClient={searchClient} indexName="dev_snippets">{children}</InstantSearch>;
+  return (
+    <InstantSearch
+      searchClient={searchClient}
+      indexName={IS_PROD ? "prod_snippets" : "dev_snippets"}
+    >
+      {children}
+    </InstantSearch>
+  );
 }
