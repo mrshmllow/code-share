3555d4c1f0a1a24b74df58a52ee345bdf46236e4
marshmallow <marshycity@gmail.com>
2023-07-11 13:23:45 +1000

    bring choose language more in line with palette

--- app/design/ChooseLanguagePopup.tsx
+++ app/design/ChooseLanguagePopup.tsx
@@ -1,9 +1,7 @@
 import { Fragment, useState } from "react";
 import { Combobox, Dialog, Transition } from "@headlessui/react";
-import Button from "@/app/design/button/Button";
 import hljs from "highlight.js";
-import { CheckIcon } from "@heroicons/react/24/outline";
-import TextInput from "./form/TextInput";
+import { CheckIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 const languages = hljs.listLanguages();
 
@@ -19,11 +17,6 @@ export default function ChooseLanguagePopup({
   onPickLanguage: (language: string) => Promise<void>;
 }) {
   const [isBusy, setIsBusy] = useState(false);
-
-  const [selectedLanguage, setSelectedLanguage] = useState(
-    initalLanguage ? initalLanguage : languages[0],
-  );
-
   const [query, setQuery] = useState("");
 
   const filteredLanguages =
@@ -33,12 +26,13 @@ export default function ChooseLanguagePopup({
           return lang.toLowerCase().includes(query.toLowerCase());
         });
 
-  const pickLanguage = async () => {
-    if (selectedLanguage !== undefined) {
+  const pickLanguage = async (language: string | null) => {
+    if (language !== null) {
       setIsBusy(true);
-      await onPickLanguage(selectedLanguage);
+      await onPickLanguage(language);
       setIsBusy(false);
     }
+
     thenFinally();
   };
 
@@ -58,7 +52,7 @@ export default function ChooseLanguagePopup({
         </Transition.Child>
 
         <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <div className="flex pt-14 justify-center p-4 text-center">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -68,86 +62,88 @@ export default function ChooseLanguagePopup({
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-md transform rounded-2xl bg-white p-6 text-center align-middle shadow-xl transition-all">
-                <Dialog.Title
-                  as="h3"
-                  className="text-lg font-medium leading-6 text-gray-900"
-                >
-                  Pick Language
-                </Dialog.Title>
-
-                <div className="mt-4 flex gap-2 w-full flex-col">
+              <Dialog.Panel className="w-full max-w-xl transform rounded-2xl bg-white text-center align-middle shadow-xl transition-all">
+                <div className="flex w-full flex-col">
                   <Combobox
-                    value={selectedLanguage}
-                    onChange={setSelectedLanguage}
+                    defaultValue={initalLanguage}
+                    onChange={async (value) => {
+                      await pickLanguage(value);
+                    }}
                   >
-                    <div className="relative">
-                      <div className="">
+                    <div className="flex items-center gap-2 pl-5 border-b border-b-gray-100 p-4">
+                      <MagnifyingGlassIcon className="w-5 h-5" />
+
+                      <div className="flex w-full">
+                        <div className="flex">
+                          <Dialog.Title as="h3">Change Language</Dialog.Title>
+
+                          <span
+                            aria-hidden="true"
+                            className="px-2 text-gray-500"
+                          >
+                            /
+                          </span>
+                        </div>
+
                         <Combobox.Input
-                          as={TextInput}
-                          full={true}
                           onChange={(event) => setQuery(event.target.value)}
+                          autoFocus={true}
+                          className="outline-none"
+                          placeholder="Search languages..."
+                          autoComplete="off"
+                          autoCorrect="off"
+                          autoCapitalize="off"
+                          spellCheck={false}
+                          aria-label="Language search "
                         />
                       </div>
-
-                      <Combobox.Options className="absolute max-h-60 overflow-auto rounded-md py-2 shadow-lg text-sm z-10 bg-white w-full">
-                        {filteredLanguages.length === 0 && query !== "" ? (
-                          <p>Nothing Found</p>
-                        ) : (
-                          filteredLanguages.map((language) => (
-                            <Combobox.Option
-                              key={language}
-                              value={language}
-                              className={({ active }) =>
-                                `relative cursor-default select-none py-2 pl-10 pr-4 text-left ${
-                                  active
-                                    ? "bg-indigo-500 text-white"
-                                    : "text-gray-900"
-                                }`
-                              }
-                            >
-                              {({ active, selected }) => (
-                                <>
-                                  <span
-                                    className={`block truncate ${
-                                      selected ? "font-medium" : "font-normal"
-                                    }`}
-                                  >
-                                    {language}
-                                  </span>
-                                  {selected ? (
-                                    <span
-                                      className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                                        active
-                                          ? "text-white"
-                                          : "text-indigo-500"
-                                      }`}
-                                    >
-                                      <CheckIcon
-                                        className="h-5 w-5"
-                                        aria-hidden="true"
-                                      />
-                                    </span>
-                                  ) : null}
-                                </>
-                              )}
-                            </Combobox.Option>
-                          ))
-                        )}
-                      </Combobox.Options>
                     </div>
-                  </Combobox>
 
-                  <Button
-                    full
-                    isBusy={isBusy}
-                    onClick={async () => await pickLanguage()}
-                  >
-                    <Button.Icon />
-                    <Button.Text busyText={["Updat", "ing"]}>
-                      Update
-                    </Button.Text>
-                  </Button>
+                    <Combobox.Options className="max-h-80 rounded-b-2xl overflow-auto p-2 z-10 bg-white w-full">
+                      {filteredLanguages.length === 0 && query !== "" ? (
+                        <p>Nothing Found</p>
+                      ) : (
+                        filteredLanguages.map((language) => (
+                          <Combobox.Option
+                            key={language}
+                            value={language}
+                            className={({ active }) =>
+                              `relative flex justify-between cursor-default rounded-lg select-none py-2 pl-10 pr-4 text-left ${
+                                active ? "bg-gray-100" : "text-gray-900"
+                              }`
+                            }
+                          >
+                            {({ active, selected }) => (
+                              <>
+                                <span
+                                  className={`block truncate ${
+                                    selected ? "font-medium" : "font-normal"
+                                  }`}
+                                >
+                                  {language}
+                                </span>
+
+                                {selected ? (
+                                  <span
+                                    className={`absolute inset-y-0 left-0 flex items-center pl-3 text-gray-400`}
+                                    aria-hidden="true"
+                                  >
+                                    <CheckIcon className="h-5 w-5" />
+                                  </span>
+                                ) : null}
+
+                                {active && (
+                                  <span className="text-gray-600">
+                                    Choose Language
+                                  </span>
+                                )}
+                              </>
+                            )}
+                          </Combobox.Option>
+                        ))
+                      )}
+                    </Combobox.Options>
+                  </Combobox>
                 </div>
               </Dialog.Panel>
             </Transition.Child>
