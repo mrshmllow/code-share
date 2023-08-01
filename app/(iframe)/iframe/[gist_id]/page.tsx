b9624b5d0da7f037d844f32373e8bca81d95fe91
marshmallow <marshycity@gmail.com>
2023-08-01 12:49:36 +1000

    iframe fill document properly

--- app/(iframe)/iframe/[gist_id]/page.tsx
+++ app/(iframe)/iframe/[gist_id]/page.tsx
@@ -4,19 +4,28 @@ import { NameContent } from "@/app/design/NameContent";
 import ButtonishLink from "@/app/design/button/ButtonishLink";
 import { useiFrameStore } from "./store";
 import "@catppuccin/highlightjs/sass/catppuccin-latte.scss";
+import { cx } from "cva";
 
-export default function GistIframePage() {
+export default function GistIframePage({
+  searchParams: { noShare },
+}: {
+  searchParams: {
+    noShare?: string;
+  };
+}) {
   const gist = useiFrameStore();
 
+  console.log("NOSHARE", noShare);
+
   return (
-    <div className="border border-gray-300 rounded-lg">
+    <div className="border border-gray-300 rounded-lg min-h-screen flex flex-col">
       <div className="border-b border-gray-300 p-2 flex justify-between">
         <p className="inline-flex items-center gap-4 px-4 py-2 font-mono text-gray-700">
           <NameContent aiCompleted={gist.aiCompleted} name={gist.name} />
         </p>
       </div>
 
-      <div className="flex flex-row p-2 overflow-auto">
+      <div className="flex flex-row p-2 overflow-auto grow">
         <pre className="flex flex-col font-mono px-4">
           {[...Array((gist.text.match(/\n/g) || "").length + 1)].map((_, i) => (
             <a href={`#L${i + 1}`} key={i} className="" id={`L${i + 1}`}>
@@ -34,17 +43,24 @@ export default function GistIframePage() {
         </pre>
       </div>
 
-      <div className="border-t border-gray-300 bg-gray-100 p-2 flex justify-between items-center">
-        <ButtonishLink
-          href={`https://snip.cafe/${gist.owner}/${gist.id}`}
-          target="_blank"
-          intent="secondary"
-          rel="noopener noreferrer"
-        >
-          View on snip.cafe
-        </ButtonishLink>
+      <div
+        className={cx(
+          "border-t border-gray-300 bg-gray-100 p-2 flex items-center",
+          noShare === undefined ? "justify-between" : "justify-end"
+        )}
+      >
+        {noShare === undefined ? (
+          <ButtonishLink
+            href={`https://snip.cafe/${gist.owner}/${gist.id}`}
+            target="_blank"
+            intent="secondary"
+            rel="noopener noreferrer"
+          >
+            View on snip.cafe
+          </ButtonishLink>
+        ) : null}
 
-        <>{gist.language && <p>{gist.language}</p>}</>
+        <>{gist.language && <p className="">{gist.language}</p>}</>
       </div>
     </div>
   );
